a885c2892dd612ef4d20b262de81453e06771455
Darrell <176396718+buheracoupe@users.noreply.github.com>
2024-08-30 19:28:53 +0200

    Added findMatches function with regular expression matching for city and state properties

---
+++ typeahead.js
@@ -0,0 +1,26 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const searchInput = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
+
+const cities = [];
+fetch(endpoint)
+.then(response => response.json())
+.then(data => {cities.push(...data)
+const matches = findMatches("New", cities);
+    console.log(matches);});
+
+ console.log(cities)
+
+const findMatches = (matchText, cities) => {
+    const regex = new RegExp(matchText, "i");
+    return cities.filter((place) => {
+    return place.city.match(regex) || place.state.match(regex)
+    })
+}
+
+
+const displayMatches = () => {
+
+}
+
+// searchElement.addEventListener("input", displayArr)
